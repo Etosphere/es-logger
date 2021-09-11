c1cc9b201f9544fcf69a96ae068d90608fec3568
etosphere <etosphere@outlook.com>
2021-09-11 14:30:00 -0400

    Add dark mode toggle button, change appbar style

--- src/App.js
+++ src/App.js
@@ -1,15 +1,40 @@
-import React, {Suspense} from 'react';
-import createTheme from "@material-ui/core/styles/createTheme";
-import {ThemeProvider} from "@material-ui/core/styles";
-import CssBaseline from "@material-ui/core/CssBaseline";
-import Container from "@material-ui/core/Container";
-import Link from "@material-ui/core/Link";
+import React, {Suspense, useState} from 'react';
+import createTheme from '@material-ui/core/styles/createTheme';
+import {ThemeProvider} from '@material-ui/core/styles';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Container from '@material-ui/core/Container';
+import Link from '@material-ui/core/Link';
+import Paper from '@material-ui/core/Paper';
 import Backdrop from '@material-ui/core/Backdrop';
 import CircularProgress from '@material-ui/core/CircularProgress';
 import GitHubIcon from '@material-ui/icons/GitHub';
 import TopAppBar from './TopAppBar';
 
-const theme = createTheme({
+const lightModePalette = {
+  palette: {
+    type: "light",
+    primary: {
+      main: "#717bd6",
+    },
+    secondary: {
+      main: "#e8618c",
+    },
+  }
+};
+
+const darkModePalette = {
+  palette: {
+    type: "dark",
+    primary: {
+      main: "#717bd6",
+    },
+    secondary: {
+      main: "#e8618c",
+    },
+  }
+};
+
+const globalTheme = createTheme({
   overrides: {
     MuiCssBaseline: {
       "@global": {
@@ -42,14 +67,6 @@ const theme = createTheme({
       }
     }
   },
-  palette: {
-    primary: {
-      main: "#717bd6",
-    },
-    secondary: {
-      main: "#e8618c",
-    },
-  },
 });
 
 const LogParserComponent = React.lazy(() => import('./LogParser'));
@@ -69,17 +86,24 @@ const SuspenseComponent = () => (
 )
 
 function App() {
+  const [useDarkMode, setDarkMode] = useState(false);
+  const darkModeTheme = createTheme(useDarkMode ? darkModePalette : lightModePalette);
+
   return (
-    <ThemeProvider theme={theme}>
+    <ThemeProvider theme={globalTheme}>
       <CssBaseline/>
-      <TopAppBar/>
-      <SuspenseComponent/>
-      <footer id="footer" className="footer">
-        <Link href="https://github.com/etosphere/es-logger" style={{color: "#907da2"}}>
-          <GitHubIcon fontSize="small"/>
-        </Link>
-        <div style={{color: "#907da2"}}>©&nbsp;2021&nbsp;ES</div>
-      </footer>
+      <ThemeProvider theme={darkModeTheme}>
+        <Paper>
+          <TopAppBar useDarkMode={useDarkMode} toggleDarkMode={() => setDarkMode(!useDarkMode)}/>
+          <SuspenseComponent/>
+          <footer id="footer" className="footer">
+            <Link href="https://github.com/etosphere/es-logger" style={{color: "#907da2"}}>
+              <GitHubIcon fontSize="small"/>
+            </Link>
+            <div style={{color: "#907da2"}}>©&nbsp;2021&nbsp;ES</div>
+          </footer>
+        </Paper>
+      </ThemeProvider>
     </ThemeProvider>
   );
 }
--- src/TopAppBar.js
+++ src/TopAppBar.js
@@ -1,8 +1,11 @@
-import AppBar from "@material-ui/core/AppBar";
-import Toolbar from "@material-ui/core/Toolbar";
-import Typography from "@material-ui/core/Typography";
-import useScrollTrigger from "@material-ui/core/useScrollTrigger";
-import Slide from "@material-ui/core/Slide";
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import useScrollTrigger from '@material-ui/core/useScrollTrigger';
+import Slide from '@material-ui/core/Slide';
+import Brightness3Icon from '@material-ui/icons/Brightness3';
+import Brightness7Icon from '@material-ui/icons/Brightness7';
+import IconButton from '@material-ui/core/IconButton';
 
 function HideOnScroll(props) {
   const { children } = props;
@@ -19,13 +22,22 @@ function HideOnScroll(props) {
 }
 
 export default function TopAppBar(props) {
+  const darkModeIcon = props.useDarkMode ? <Brightness3Icon/> : <Brightness7Icon/>
+
   return (
     <>
       <HideOnScroll {...props}>
-        <AppBar style={{backgroundColor: "#f7d774", color: "#5f5f5f"}}>
-        {/* <AppBar elevation={0} color="transparent"> */}
-          <Toolbar>
-            <Typography variant="h6">ES Logger</Typography>
+        {/*<AppBar style={{backgroundColor: "#f7d774"}}>*/}
+         <AppBar elevation={0} color="transparent">
+          <Toolbar style={{paddingLeft: '24px', paddingRight: '24px'}}>
+            <Typography variant="h6" style={{flex: 1}}>ES Logger</Typography>
+            <IconButton
+              edge="end"
+              aria-label="dark mode toggle icon"
+              color="inherit"
+              onClick={props.toggleDarkMode}>
+              {darkModeIcon}
+            </IconButton>
           </Toolbar>
         </AppBar>
       </HideOnScroll>
